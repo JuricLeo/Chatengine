419e9e22e7c182fa4e9b82a5c858477a86ae4fe7
JuricLeo <ljurickc@gmail.com>
2023-07-05 14:59:06 +0200

    chatengine

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="48d21bbb-ebde-48e7-9598-8b95235dd3d7"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
